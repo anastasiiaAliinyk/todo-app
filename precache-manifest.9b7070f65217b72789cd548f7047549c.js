6fd2e84ef856f8e31861c0d1816edab142c78502
Anastasiia Aliinyk <anastasiia.aliinyk@gmail.com>
2021-05-05 13:21:27 +0300

    Updates

---
+++ precache-manifest.9b7070f65217b72789cd548f7047549c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76237b07886d1e4594d4e2429011f36a",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "10cf5de3a73d8be24305",
+    "url": "/todo-app/static/css/main.eca640dc.chunk.css"
+  },
+  {
+    "revision": "d0f56f7b366ef46ab77b",
+    "url": "/todo-app/static/js/2.f459cf2a.chunk.js"
+  },
+  {
+    "revision": "10cf5de3a73d8be24305",
+    "url": "/todo-app/static/js/main.3d72375f.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
